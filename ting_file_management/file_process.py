745e31d75cc80666f41e273f4ba94f683940f7ff
brunaumLL <bruno99louzada@gmail.com>
2022-12-06 18:00:20 -0300

    req 03 feito

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    txt = txt_importer(path_file)
+    data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(txt),
+            "linhas_do_arquivo": (txt),
+        }
+    instance.enqueue(data)
+    sys.stdout.write(str(data))
+    return data
 
 
 def remove(instance):
