06af2add6bec6ae977bfb649b890d2ac1e123176
brunaumLL <bruno99louzada@gmail.com>
2022-12-07 14:15:10 -0300

    req 07 feito

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -19,4 +19,21 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+    result = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+        lines = file['linhas_do_arquivo']
+        for index in range(len(lines)):
+            if word.lower() in lines[index].lower():
+                occurrences.append({'linha': index + 1, 'conteudo': lines[index]})
+
+        
+        if len(occurrences) >= 1:
+            result.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occurrences
+            })
+
+    return result
